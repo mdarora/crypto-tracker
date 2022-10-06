ea55e72ca192657b0e377f5d66072164893e2b4a
mdarora <mdarora0001@gmail.com>
2022-10-06 06:23:16 -0400

    fetching data and showing all coins

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Coin from './components/Coin';
 
 function App() {
-  
+  const apiUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+
+  const [coins, setCoins] = useState([]);
+
+  useEffect(()=>{
+
+    axios.get(apiUrl).then((res)=>{
+      setCoins(res.data);
+      console.log(res.data);
+    }).catch((err) => console.log(err));
+
+  }, []);
+
   return (
-    <div className="App">
-      App
-    </div>
+    <section className="App">
+      <div className="container">
+        <div className="search">
+          <h1>Search a currency</h1>
+          <input type="text" placeholder='search' />
+        </div>
+
+        <div className="coins">
+          <table>
+            <thead>
+              <tr>
+                <td>Coin</td>
+                <td>Symbol</td>
+                <td>Price</td>
+                <td>Volume</td>
+                <td>24h Change</td>
+                <td>Market Cap</td>
+              </tr>
+            </thead>
+            <tbody>
+              {coins.map((coin, i) =><Coin key={coin.symbol} name={coin.name} image={coin.image} symbol={coin.symbol.toUpperCase()} price={coin.current_price} volume={coin.total_volume} mktCap={coin.market_cap} priceChange={coin.price_change_percentage_24h} />)} 
+
+            </tbody>
+          </table>
+        </div>
+
+      </div>
+    </section>
   );
 }
 
