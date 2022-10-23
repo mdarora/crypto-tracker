906809dfc11af404e86c31b669c3e07a6e7b1fe1
mdarora <mdarora0001@gmail.com>
2022-10-23 15:40:41 -0400

    Added select option to change currency

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({currency}) => {
   return (
     <>
     <nav>
         <h1 className="logo">Crypto-Tracker</h1>
+        <div className="nav-menu">
+            <select name="currency" onChange={e => currency.setCurrency(e.target.value)}>
+                <option value="USD">USD</option>   
+                <option value="CAD">CAD</option>   
+                <option value="INR">INR</option>   
+            </select>
+        </div>
     </nav>
     </>
   )
