e96ae1f44776781989291dee0aab5032cafc5692
mdarora <mdarora0001@gmail.com>
2022-10-23 15:34:09 -0400

    Added astate for currency

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Coin from './components/Coin';
 import Header from './components/Header';
 
 function App() {
-  const apiUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=50&page=1&sparkline=false";
+  const [currency, setCurrency] = useState('USD');
+  const apiUrl = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=50&page=1&sparkline=false`;
 
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState('');
@@ -17,7 +18,7 @@ function App() {
       setCoins(res.data);
     }).catch((err) => console.log(err));
 
-  }, []);
+  }, [apiUrl]);
 
   const filteredCoins = coins.filter(coin => {
     return coin.name.toLowerCase().includes(search.toLowerCase());
